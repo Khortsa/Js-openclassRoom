c2c53505805f81c4c07055ba8cda82adfbcfa644
Tenzin Choezom <khortsa2021@gmail.com>
2022-12-20 11:24:11 +0000

    second modification added

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ let numberOfSeasons = 6;
 let numberOfEpisodes = 12;
 
 // ==========================================
+let episodeTime = 45;
+let commercialTime = 5;
+let totalShowTime = (episodeTime + commercialTime) * numberOfSeasons * numberOfEpisodes;
+
+// let paragraph = document.querySelector('#info');
+// paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
 
 let paragraph = document.querySelector('#info');
-paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season
+
+Total viewing time: ${totalShowTime} minutes`
